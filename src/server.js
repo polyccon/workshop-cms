34dbe8354d60557906034297939992c494199905
Constantina <polyccon@gmail.com>
2017-07-18 11:55:28 +0100

    updatefromTuesday

--- src/server.js
+++ src/server.js
@@ -11,44 +11,50 @@ var contentType = {
   jpg: "image/jpeg",
   png: "image/png"
 };
-function handler (request, response) {
+
+function handler(request, response) {
   var endpoint = request.url;
   console.log(endpoint);
   var extension = endpoint.split('.')[1];
 
-  if (endpoint === "/"){
-    response.writeHead(200, {"Content-Type": "text/html"});
+  if (endpoint === "/") {
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
 
-    fs.readFile(__dirname + "/../public/index.html", function(error, file){
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
       if (error) {
         console.log(error);
         return;
       }
       response.end(file);
     });
-  }
-  else if{
-    if (url.indexOf(public)!= -1){
-      response.writeHead(200, {"Content-Type": contentType[extension]});
+  } else if (endpoint === "/node") {
+    message = "You requested node";
+  // var method = request.method;
+     console.log(method);
+     response.writeHead(200, {"Content-Type": "text/html"});
+     response.write(message);
+     response.end();
+
+  } else if (endpoint === "/girls") {
+    message = "we are node girls";
+    console.log(method);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+  } else if {
+    if (url.indexOf(public) != -1) {
+      response.writeHead(200, {
+        "Content-Type": contentType[extension]
+      });
     }
   }
 }
-//   if (endpoint === "/node"){
-//     message = "You requested node";
-//   }
-//   else if (endpoint === "/girls"){
-//     message = "we are node girls";
-//   }
-//   var method = request.method;
-//   console.log(method);
-//   response.writeHead(200, {"Content-Type": "text/html"});
-//   response.write(message);
-//   response.end();
-// }
 
 var server = http.createServer(handler);
 
-server.listen(3000, function (){
+server.listen(3000, function() {
 
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
